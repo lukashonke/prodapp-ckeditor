807d3d02777311ffeb0069781e1192e5f9fd8549
Lukas Honke <lukas.honke@newtonmedia.cz>
2022-10-24 17:32:17 +0200

    updated from ckeditor

--- README.md
+++ README.md
@@ -57,7 +57,7 @@ Follow the guides available on https://ckeditor.com/docs/ckeditor5/latest/framew
 ## FAQ
 | Where is the place to report bugs and feature requests?
 
-You can create an issue on https://github.com/ckeditor/ckeditor5/issues including the build id - `50iqe11eft06-k40mjx9ssgdv`. Make sure that the question / problem is unique, please look for a possibly asked questions in the search box. Duplicates will be closed.
+You can create an issue on https://github.com/ckeditor/ckeditor5/issues including the build id - `2spm0endpc1f-vybv0dladqfb`. Make sure that the question / problem is unique, please look for a possibly asked questions in the search box. Duplicates will be closed.
 
 | Where can I learn more about the CKEditor 5 framework?
 
--- src/ckeditor.js
+++ src/ckeditor.js
@@ -6,37 +6,33 @@ import BalloonBlockEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooned
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
 import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
 import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
 import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
-import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert.js';
 import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
 import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableCaption from '@ckeditor/ckeditor5-table/src/tablecaption.js';
-import TableColumnResize from '@ckeditor/ckeditor5-table/src/tablecolumnresize.js';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
@@ -49,37 +45,33 @@ Editor.builtinPlugins = [
 	Autoformat,
 	AutoImage,
 	AutoLink,
+	Autosave,
 	BlockQuote,
 	BlockToolbar,
 	Bold,
-	CloudServices,
 	Code,
 	CodeBlock,
 	Essentials,
-	FontColor,
-	FontSize,
 	Heading,
+	Highlight,
 	HorizontalLine,
 	Image,
 	ImageInsert,
 	ImageResize,
-	ImageStyle,
-	ImageToolbar,
 	ImageUpload,
 	Indent,
 	IndentBlock,
 	Italic,
 	Link,
 	List,
+	Markdown,
 	MediaEmbed,
 	Paragraph,
-	PasteFromOffice,
 	RemoveFormat,
 	SimpleUploadAdapter,
+	SourceEditing,
 	Strikethrough,
 	Table,
-	TableCaption,
-	TableColumnResize,
 	TableToolbar,
 	TextTransformation,
 	TodoList,
@@ -90,45 +82,41 @@ Editor.builtinPlugins = [
 Editor.defaultConfig = {
 	toolbar: {
 		items: [
-			'heading',
 			'bold',
 			'italic',
 			'underline',
 			'strikethrough',
-			'|',
-			'link',
 			'code',
 			'|',
+			'link',
+			'highlight',
+			'-',
 			'indent',
 			'outdent',
-			'|',
-			'fontSize',
 			'removeFormat'
-		]
+		],
+		shouldNotGroupWhenFull: true
 	},
 	language: 'en',
 	blockToolbar: [
+		'heading',
+		'|',
 		'bulletedList',
 		'numberedList',
 		'todoList',
 		'|',
+		'horizontalLine',
 		'insertTable',
+		'imageInsert',
+		'mediaEmbed',
 		'codeBlock',
 		'blockQuote',
-		'mediaEmbed',
-		'imageUpload',
-		'imageInsert',
 		'|',
-		'horizontalLine'
+		'sourceEditing',
+		'|',
+		'undo',
+		'redo'
 	],
-	image: {
-		toolbar: [
-			'imageTextAlternative',
-			'imageStyle:inline',
-			'imageStyle:block',
-			'imageStyle:side'
-		]
-	},
 	table: {
 		contentToolbar: [
 			'tableColumn',
